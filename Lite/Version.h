7a4385d31bd3ab847ca1f64177c576602657bbb3
ceshine <ceshine@ceshine.net>
2008-12-31 01:56:47 +0000

    Reverse to 366

--- Lite/Version.h
+++ Lite/Version.h
@@ -1,36 +1,36 @@
 //Generated from Version_Create.bat "Version_Set_Lite.bat" 
 // 
-#define FILEVER            9,0,1,4 
-#define FILEVER_STRA       "9,0,1,4\0" 
-#define FILEVER_DOT_STRA       "9.0.1.4\0" 
-#define FILEVER_DOT_STRW       L"9.0.1.4\0" 
+#define FILEVER            8,2,0,363 
+#define FILEVER_STRA       "8,2,0,363\0" 
+#define FILEVER_DOT_STRA       "8.2.0.363\0" 
+#define FILEVER_DOT_STRW       L"8.2.0.363\0" 
 // 
-#define PRODUCTVER         9,0,1,4 
-#define PRODUCTVER_STRA    "9,0,1,4\0" 
-#define PRODUCTVER_DOT_STRA    "9.0.1.4\0" 
-#define PRODUCTVER_DOT_STRW    L"9.0.1.4\0" 
+#define PRODUCTVER         8,2,0,363 
+#define PRODUCTVER_STRA    "8,2,0,363\0" 
+#define PRODUCTVER_DOT_STRA    "8.2.0.363\0" 
+#define PRODUCTVER_DOT_STRW    L"8.2.0.363\0" 
 // 
-#define Version_Major 9 
-#define Version_Minor 0 
-#define Version_PatchLevel 1 
-#define Version_BuildNumber   4 
+#define Version_Major 8 
+#define Version_Minor 2 
+#define Version_PatchLevel 0 
+#define Version_BuildNumber   363 
 // 
-#define Version_RevDateA   "2008/12/04 14:21:29" 
-#define Version_RevDateW   "2008/12/04 14:21:29" 
+#define Version_RevDateA   "2008/12/04 11:39:35" 
+#define Version_RevDateW   L"2008/12/04 11:39:35" 
 // 
 #define Version_UseSvnBuildNumber   1 
 // 
-#define Version_CompanyNameA   "PCMan CE Project\0" 
-#define Version_CompanyNameW   L"PCMan CE Project\0" 
-#define Version_ProductNameA   "PCMan 2009 (Beta)\0" 
-#define Version_ProductNameW   L"PCMan 2009 (Beta)\0" 
+#define Version_CompanyNameA   "Open PCMan Project\0" 
+#define Version_CompanyNameW   L"Open PCMan Project\0" 
+#define Version_ProductNameA   "PCMan 2007\0" 
+#define Version_ProductNameW   L"PCMan 2007\0" 
 #define Version_InternalNameA   "Open PCMan\0" 
 #define Version_InternalNameW   L"Open PCMan\0" 
 #define Version_LegalCopyrightA   "Copyright (C) 2001 - 2007 由 洪任諭及 Open PCMan Team 共同製作\0" 
 #define Version_LegalCopyrightW   L"Copyright (C) 2001 - 2007 由 洪任諭及 Open PCMan Team 共同製作\0" 
 #define Version_OriginalFilenameA   "PCMan.exe\0" 
 #define Version_OriginalFilenameW   L"PCMan.exe\0" 
-#define Version_FileDescriptionA   "PCMan 2009 (Beta)\0" 
-#define Version_FileDescriptionW   L"PCMan 2009 (Beta)\0" 
+#define Version_FileDescriptionA   "PCMan 2007\0" 
+#define Version_FileDescriptionW   L"PCMan 2007\0" 
 #define Version_CommentsA   "本軟體由洪任諭及 Open PCMan Team 共同製作." " Revision Date: " Version_RevDateA 
 #define Version_CommentsW   L"本軟體由洪任諭及 Open PCMan Team 共同製作." " Revision Date: " Version_RevDateA 
