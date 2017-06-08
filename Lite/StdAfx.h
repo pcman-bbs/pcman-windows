12c6e6e739e4092dd52840f2ceb3dd3bfda9b03e
Robert <robert@arctic.tw>
2017-06-09 00:19:57 +0800

    Upgrade to VS 2017.
    * Fix build.
    * Set minimum requirement to Win XP.
    * Tested Debug and Release build for Combo and Lite.
    
    Projects broken:
    * Base64Test
    * UAOTabGen

--- Lite/StdAfx.h
+++ Lite/StdAfx.h
@@ -13,7 +13,7 @@
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 
 #ifndef _WIN32_WINNT
-#define _WIN32_WINNT  0x0500
+#define _WIN32_WINNT  0x0501
 #endif
 
 // For advanced menu functionality (Win 98 or lator)
