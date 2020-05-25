1f093b495f7d5894e2f4aed61ba4407729d94e21
Robert <robert@arctic.tw>
2020-05-25 11:53:56 -0700

    Properly handle async task failures.
    This fix crashing when websocket connection failed.
    
    Fixes #47.

--- Lite/Websocket.cpp
+++ Lite/Websocket.cpp
@@ -16,6 +16,13 @@ using web::websockets::client::websocket_outgoing_message;
 using web::websockets::client::websocket_close_status;
 using web::websockets::client::websocket_message_type;
 
+enum class WebsocketState {
+	NEW = 0,
+	CONNECTING = 1,
+	ESTABLISHED = 2,
+	CLOSED = 3,
+};
+
 class CWebsocket
 	: public std::enable_shared_from_this<CWebsocket>
 	, public CConnIO
@@ -34,12 +41,14 @@ public:
 	std::string Description() const override { return ""; }
 
 private:
+	void HandleConnectDone(pplx::task<void> task);
 	void StartReceive();
+	void HandleReceiveDone(pplx::task<websocket_incoming_message> task);
 	void HandleMessage(const websocket_incoming_message& msg);
-	void HandleConnectResult(bool success);
-	void HandleClose();
-	void HandleSendDone();
+	void HandleSendDone(pplx::task<void> task);
 	void SendQueue();
+	void HandleCloseError(const std::exception& ex);
+	void HandleCloseNormal();
 
 	static websocket_client_config GetClientConfig();
 
@@ -47,7 +56,7 @@ private:
 	std::shared_ptr<CConnEventDelegate> delegate_;
 	std::atomic<bool> is_secure_ = false;
 
-	std::atomic<bool> shutdown_ = false;
+	std::atomic<WebsocketState> state_ = WebsocketState::NEW;
 	websocket_client client_;
 
 	std::mutex mu_;
@@ -73,40 +82,64 @@ websocket_client_config CWebsocket::GetClientConfig()
 
 bool CWebsocket::Connect()
 {
+	assert(state_ == WebsocketState::NEW);
+	state_ = WebsocketState::CONNECTING;
+
 	std::string url = address_.URL();
 	auto ws = shared_from_this();
 	client_.connect(web::uri(utility::conversions::to_string_t(url))).then(
 		[ws](pplx::task<void> task) {
-			try {
-				task.get();
-				ws->HandleConnectResult(true);
-				ws->StartReceive();
-			}
-			catch (const std::exception &ex) {
-				OutputDebugString(ex.what());
-				ws->HandleConnectResult(false);
-			}
+			ws->HandleConnectDone(task);
 		});
 	return true;
+}
+
+void CWebsocket::HandleConnectDone(pplx::task<void> task)
+{
+	bool is_success = false;
+	try {
+		task.get();
+		is_success = true;
+	}
+	catch (const std::exception &ex) {
+		OutputDebugString(ex.what());
+	}
+
+	state_ = is_success ? WebsocketState::ESTABLISHED : WebsocketState::CLOSED;
+	is_secure_ = (client_.uri().scheme() == U("wss"));
+
+	delegate_->OnConnect(is_success);
+
+	if (is_success)
+		StartReceive();
 }
 
 void CWebsocket::StartReceive()
 {
 	auto ws = shared_from_this();
-	client_.receive().then(
-		[ws](pplx::task<websocket_incoming_message> task) {
-			try {
-				ws->HandleMessage(task.get());
-			}
-			catch (const std::exception &ex) {
-				OutputDebugString(ex.what());
-				ws->HandleClose();
-			}
-		});
+	client_.receive().then([ws](pplx::task<websocket_incoming_message> task) {
+		ws->HandleReceiveDone(task);
+	});
+}
+
+void CWebsocket::HandleReceiveDone(pplx::task<websocket_incoming_message> task)
+{
+	try {
+		HandleMessage(task.get());
+	}
+	catch (const std::exception &ex) {
+		HandleCloseError(ex);
+	}
 }
 
 int CWebsocket::Send(const void* data, size_t length)
 {
+	// We drop user inputs before websocket is established.
+	// So that if user smashed on keyboard during connect,
+	// they won't suddenly see craps sent out after we are connected.
+	if (state_ != WebsocketState::ESTABLISHED)
+		return length;
+
 	std::vector<uint8_t> buf(length);
 	memcpy(&buf[0], data, length);
 	bool initiate_send;
@@ -129,11 +162,22 @@ void CWebsocket::SendQueue()
 	websocket_outgoing_message msg;
 	msg.set_binary_message(
 		Concurrency::streams::container_stream<std::vector<uint8_t>>::open_istream(send_buffers_.front()));
-	client_.send(msg).then(std::bind(&CWebsocket::HandleSendDone, shared_from_this()));
+	auto ws = shared_from_this();
+	client_.send(msg).then([ws](pplx::task<void> task) {
+		ws->HandleSendDone(task);
+	});
 }
 
-void CWebsocket::HandleSendDone()
+void CWebsocket::HandleSendDone(pplx::task<void> task)
 {
+	try {
+		task.get();
+	}
+	catch (const std::exception &ex) {
+		HandleCloseError(ex);
+		return;
+	}
+
 	{
 		std::lock_guard<std::mutex> lk(mu_);
 		send_buffers_.pop_front();
@@ -148,12 +192,28 @@ void CWebsocket::Shutdown()
 
 void CWebsocket::Close()
 {
-	if (shutdown_.exchange(true))
+	if (state_.exchange(WebsocketState::CLOSED) == WebsocketState::CLOSED)
 		return;
 	auto ws = shared_from_this();
-	client_.close().then([ws]() {
+	client_.close().then([ws](pplx::task<void> task) {
+		// We don't really care about whether task is successful.
 		ws->delegate_->OnClose();
 	});
+}
+
+void CWebsocket::HandleCloseError(const std::exception& ex)
+{
+	if (state_.exchange(WebsocketState::CLOSED) == WebsocketState::CLOSED)
+		return;
+	OutputDebugString(ex.what());
+	delegate_->OnClose();
+}
+
+void CWebsocket::HandleCloseNormal()
+{
+	if (state_.exchange(WebsocketState::CLOSED) == WebsocketState::CLOSED)
+		return;
+	delegate_->OnClose();
 }
 
 void CWebsocket::HandleMessage(const websocket_incoming_message& msg)
@@ -164,34 +224,30 @@ void CWebsocket::HandleMessage(const websocket_incoming_message& msg)
 			Concurrency::streams::container_buffer<std::vector<uint8_t>> buf;
 			const size_t length = msg.length();
 			auto ws = shared_from_this();
-			msg.body().read(buf, length).then([ws, buf](size_t n) {
-				const auto &vec = buf.collection();
-				ws->delegate_->OnReceive(&vec[0], vec.size());
+			msg.body().read(buf, length).then([ws, buf](pplx::task<size_t> task) {
+				try {
+					size_t n = task.get();
+					const auto &vec = buf.collection();
+					ws->delegate_->OnReceive(&vec[0], vec.size());
+				}
+				catch (const std::exception& ex) {
+					OutputDebugString(ex.what());
+					// Continue to receive.
+					// We should get error later if connection broke.
+				}
 				ws->StartReceive();
 			});
 			break;
 		}
 
 		case websocket_message_type::close:
-			HandleClose();
+			HandleCloseNormal();
 			break;
 
 		default:
 			StartReceive();
 			break;
 	}
-}
-
-void CWebsocket::HandleConnectResult(bool success)
-{
-	is_secure_ = (client_.uri().scheme() == U("wss"));
-
-	delegate_->OnConnect(true);
-}
-
-void CWebsocket::HandleClose()
-{
-	delegate_->OnClose();
 }
 
 std::shared_ptr<CConnIO> CreateWebsocket(
